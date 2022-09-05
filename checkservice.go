15b42cf4e4962fccdb20186dde9052b4a5c4e09d
Wolfgang Ellsässer <wollomatic@users.noreply.github.com>
2022-09-05 18:29:06 +0200

    refactor

--- checkservice.go
+++ checkservice.go
@@ -26,28 +26,21 @@ func checkService(ch chan serviceResponse, s Service, delay time.Duration) {
 		var response *http.Response
 		var err error
 		start := time.Now()
-		
-		switch s.Test {
-		case "GET":
-			response, err = httpGETcheck(s.Url, s.Status, s.Text, time.Duration(s.Timeout)*time.Millisecond)
-		case "HEAD":
-			response, err = httpHEADcheck(s.Url, s.Status, time.Duration(s.Timeout)*time.Millisecond)
-		default:
-			response=&http.Response{}
-			err = fmt.Errorf("unknown test type: %s", s.Test)
-		}
+		response, err = httpcheck(s.Url, s.Method, s.Status, s.Text, time.Duration(s.Timeout)*time.Millisecond)
 		o.requestDuration = time.Since(start)
 		o.response = *response
 		o.err = err
+		// if no error return immediately
 		if o.err == nil {
 			ch <- o
 			return
 		}
 	}
+	// if we get here, we have reached the maximum number of retries
 	ch <- o
 }
 
-func httpGETcheck(url string, stat int, str string, timeout time.Duration) (*http.Response, error) {
+func httpcheck(url string, method string, status int, text string, timeout time.Duration) (*http.Response, error) {
 	client := &http.Client{
 		Timeout: timeout,
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
@@ -55,45 +48,35 @@ func httpGETcheck(url string, stat int, str string, timeout time.Duration) (*htt
 		},
 	}
 
-	resp, err := client.Get(url)
+	var resp *http.Response
+	var err error
+	switch method {
+	case "GET":
+		resp, err = client.Get(url)
+	case "HEAD":
+		resp, err = client.Head(url)
+	default:
+		resp = &http.Response{}
+		err = fmt.Errorf("invalid method: %s", method)
+	}
 	if err != nil {
 		return &http.Response{}, err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != stat {
-		return resp, fmt.Errorf("status code does not match: got %d, want %d", resp.StatusCode, stat)
+	if resp.StatusCode != status {
+		return resp, fmt.Errorf("status code does not match: got %d, want %d", resp.StatusCode, status)
 	}
 
-	if str != "" {
+	if text != "" {
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			return resp, err
 		}
 
-		if !strings.Contains(string(body), str) {
-			return resp, fmt.Errorf("search string \"%s\" not found", str)
+		if !strings.Contains(string(body), text) {
+			return resp, fmt.Errorf("search string \"%s\" not found", text)
 		}
-	}
-
-	return resp, nil
-}
-
-func httpHEADcheck(url string, stat int, timeout time.Duration) (*http.Response, error) {
-	client := &http.Client{
-		Timeout: timeout,
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-	}
-
-	resp, err := client.Head(url)
-	if err != nil {
-		return &http.Response{}, err
-	}
-
-	if resp.StatusCode != stat {
-		return resp, fmt.Errorf("status code does not match: got %d, want %d", resp.StatusCode, stat)
 	}
 
 	return resp, nil
