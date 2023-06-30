a5e1a9666fb06dc7a2e82fbc891a447babecb0ac
wollomatic <67168186+wollomatic@users.noreply.github.com>
2023-06-30 14:25:03 +0200

    change error text formatting

--- pkg/httpcheck/readyamlfile.go
+++ pkg/httpcheck/readyamlfile.go
@@ -27,13 +27,13 @@ func ReadYamlFile(filename string) (Catalog, error) {
 	// check input data for needed values and fill missing optional values with defaults
 	for i, s := range sc.Services {
 		if s.Name == "" {
-			return Catalog{}, fmt.Errorf("Service #%v: name may not be empty\n", i+1)
+			return Catalog{}, fmt.Errorf("service #%v: name may not be empty", i+1)
 		}
 		if s.Url == "" {
-			return Catalog{}, fmt.Errorf("URL of Service %s may not be empty\n", s.Name)
+			return Catalog{}, fmt.Errorf("URL of Service %s may not be empty", s.Name)
 		}
 		if _, err := url.ParseRequestURI(s.Url); err != nil {
-			return Catalog{}, fmt.Errorf("URL of Service %s is invalid: %v\n", s.Name, err)
+			return Catalog{}, fmt.Errorf("URL of Service %s is invalid: %v", s.Name, err)
 		}
 		if s.Method == "" {
 			sc.Services[i].Method = serviceDefaults.Method
@@ -41,13 +41,13 @@ func ReadYamlFile(filename string) (Catalog, error) {
 		sc.Services[i].Method = strings.ToUpper(sc.Services[i].Method)
 		// check if String is in allowedMethods
 		if !strings.Contains(allowedTests, s.Method) {
-			return Catalog{}, fmt.Errorf("test \"%s\" of Service %s is not allowed. Allowed tests are: %v\n", s.Method, s.Name, allowedTests)
+			return Catalog{}, fmt.Errorf("test \"%s\" of Service %s is not allowed. Allowed tests are: %v", s.Method, s.Name, allowedTests)
 		}
 		if s.Method == "HEAD" && s.SearchText != "" {
-			return Catalog{}, fmt.Errorf("text \"%s\" of Service %s (HEAD) is not allowed. Text is only allowed for GET/POST test\n", s.SearchText, s.Name)
+			return Catalog{}, fmt.Errorf("searchtext \"%s\" of Service %s (HEAD) is not allowed. Text is only allowed for GET/POST test", s.SearchText, s.Name)
 		}
 		if s.RequestBody != "" && s.Method != "POST" {
-			return Catalog{}, fmt.Errorf("requestbody \"%s\" of Service %s is not allowed. Requestbody is only allowed for POST test\n", s.RequestBody, s.Name)
+			return Catalog{}, fmt.Errorf("requestbody \"%s\" of Service %s is not allowed. Requestbody is only allowed for POST test", s.RequestBody, s.Name)
 		}
 		if s.Status == 0 {
 			sc.Services[i].Status = serviceDefaults.Status
